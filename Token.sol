d88aa8926edaa5bd48c24bc2ca30416648b144cc
MALINGA <104059677+malinga-DIG@users.noreply.github.com>
2022-11-08 11:52:07 +0530

    Create Token.sol

---
+++ Token.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract WildThunderToken is ERC20, ERC20Snapshot, Ownable, ERC20Permit, ERC20Votes {
+    uint256 public constant MAX_SUPPLY = 960000000 * 10**18;
+    constructor()
+        ERC20("Wild Thunder Token", "WTT")
+        ERC20Permit("Wild Thunder Token")
+    {}
+
+    function snapshot() public onlyOwner {
+        _snapshot();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        require(totalSupply() + amount <= MAX_SUPPLY, "Mint would exceed MAX SUPPLY");
+        _mint(to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Snapshot)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
+}
